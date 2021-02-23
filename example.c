c1b3d36cd3968343d4712b58399d997eb3cef382
mishapsalt <79536157+mishapsalt@users.noreply.github.com>
2021-02-24 04:02:56 +1000

    Create example.c

---
+++ example.c
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <cstdio>
+#include <stdlib.h>
+#include <locale>
+#include <stdbool.h>
+
+void Error() {
+	printf("Ошибка ввода.\n");
+	exit(EXIT_FAILURE);
+}
+
+int IntInput() {
+	int a;
+	if (scanf("%d", &a) != 1) Error();
+	return a;
+}
+
+void ArrayInput(double *array, int n) {
+	printf("Введите %d чисел массива: ", n);
+	for (int i = 0; i < n; i++) {
+		array[i] = IntInput();
+	}
+}
+
+bool countLocalMin(double x, double b, double c) {
+	if ((b < x) && (b < c)) {
+		return true;
+	}
+	return false;
+}
+
+int countLocalMin(double *a, int n) {
+	for (int i = 1; i < n; i++) {
+		if (localmin(a[i], a[i + 1], a[i + 1 + 1])) {
+			int count;
+			count++;
+		}
+	}
+	return count;
+}
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	printf("Введите длину массива: ");
+	int n = IntInput();
+	if (n < 0) Error();
+	double array[100];
+	ArrayInput(array, n);
+	int k = countLocalMin(array, n);
+	printf("Результат = %d локальных минимумов.\n", k);
+	system("pause");
+}
